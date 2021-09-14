6e46c6cc0f2889eca33667e11fe2271d069102fb
Haigaz Soghmonian <haigaz@macs-MacBook-Pro.local>
2021-09-14 12:43:17 +0400

    Login Page style finalization and dashboard initialization

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,34 +4,63 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
 import Box from '@material-ui/core/Box';
+import FormControl from '@material-ui/core/FormControl';
+import Paper from "@material-ui/core/Paper";
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 const useStyles = makeStyles((theme) => ({
     background:{
-        backgroundImage:"url(/assest/background.png)",
+        backgroundImage:"url(/assests/background.jpeg)",
         backgroundSize:"cover",
-        height:900
+        height: 811,
+    },
+    loginElements:{
+        float:'left',
+        marginLeft: "260px",
+        marginTop: "330px"
+    },
+    logo:{
+        fontSize:30,
+        textAlign:'center',
+        color:"#553E93",
+        fontFamily:"sans-serif"
     },
     form:{
-        marginTop:theme.spacing(8),
+        marginTop:theme.spacing(2.5),
         display:'flex',
         flexDirection:'column',
-        alignItems:'center'
+        //alignItems:'center',
 
     },
     button:{
         marginTop:theme.spacing(2),
         margin:'0 auto',
-        display:'flex'
+        display:'flex',
+        color:"#553E93",
+        height:"35px",
+        width:"140px",
+        borderColor:"#553E93"
+    },
+    signupLink:{
+        textAlign:'center',
+        marginTop:theme.spacing(2.5)
     }
 }))
 
 const Login = (props) => {
 
     const classes = useStyles();
+
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
-    
-    
+    const [visible, setVisible] = useState(false);
+
+    const toggleVisibility = () => {
+     setVisible(!visible);
+    };
 
     const handelEmail = (e) => {
         setEmail(e.target.value);
@@ -50,21 +79,20 @@ const Login = (props) => {
 
     return (
 
-<Box className = {classes.background}>
+<Paper className = {classes.background}>
 
-<div style={{
-    fontSize:35,
-    textAlign:'center',
-    color:"purple",
-    fontFamily:"sans-serif"
-}}> LOGO </div>
-   <form className={classes.form} noValidate autoComplete="off">
+  <Box className = {classes.loginElements}>
+
+  <Box className = {classes.logo} style={{
+     }}> LOGO</Box>
+
+   <FormControl className={classes.form} >
    <TextField
     id="first"
     label="Email-Address"
     variant="filled"
     margin="normal"
-    color="secondary"
+    color="#553E93"
     value={email}
     onChange ={handelEmail}
   />
@@ -74,28 +102,39 @@ const Login = (props) => {
     label="Password"
     variant="outlined"
     margin="normal"
-    color="secondary"
-    type="password"
+    color="#553E93"
+    type={visible ? 'text':'password'}
     value={password}    
     onChange= {handelPassword}
+    InputProps={{
+        endAdornment: (
+         <InputAdornment position="end">
+          <IconButton onClick={toggleVisibility}>
+           {visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+          </IconButton>
+         </InputAdornment>
+        )
+       }}
     />
 
-   </form>
+   </FormControl>
+   
    <Button
-   variant="contained"
+   variant="outlined"
    className = {classes.button}
    type = "submit"
-   color = "primary"
    onClick ={handelSubmit}
-   >log in
+   >SIGN IN
    </Button>
 
-   <Box style={{
-       textAlign:'center',
-   }}>
-   <Link  to="/signup">If you don't have an account click the sign-up link</Link>
+   <Box className = {classes.signupLink} >Don't have an account. Create account 
+   <Link  to="/signup"> here</Link> .
    </Box>
+
    </Box>
+
+   </Paper>
+
     );
 }
 
