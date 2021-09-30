d2bb43357e2ea6f70ae5453e2e9381e6d046350f
Haigaz Soghmonian <haigaz@macs-MacBook-Pro.local>
2021-09-30 12:41:07 +0400

    refactor:deleting unessasary parts of the code

--- src/components/quiz/questionsForm/QuestionsForm.js
+++ src/components/quiz/questionsForm/QuestionsForm.js
@@ -113,57 +113,7 @@ function changeOptionValue(text, i, j){
             elevation={1}
             style={{ width: "100%" }}
           >
-            {!questions[i].open ? (
-              <div className="saved_questions">
-                <Typography
-                  style={{
-                    fontSize: "15px",
-                    fontWeight: "400",
-                    letterSpacing: ".1px",
-                    lineHeight: "24px",
-                    paddingBottom: "8px",
-                  }}
-                >
-                  {i + 1}. {questions.questionText}
-                </Typography>
-                {ques.options.map((op, j) => (
-                  <div key={j}>
-                    <div style={{ display: "flex" }}>
-                      <FormControlLabel
-                        style={{ marginLeft: "5px", marginBottom: "5px" }}
-                        disabled
-                        control={
-                          <input
-                            type={ques.questionType}
-                            color="primary"
-                            style={{ marginRight: "3px" }}
-                            required={ques.type}
-                          />
-                        }
-                        label={
-                          <Typography
-                            style={{
-                              fontFamily: " Roboto,Arial,sans-serif",
-                              fontSize: " 13px",
-                              fontWeight: "400",
-                              letterSpacing: ".2px",
-                              lineHeight: "20px",
-                              color: "#202124",
-                            }}
-                          >
-                            {ques.options[j].optionText}
-                          </Typography>
-                        }
-                      />
-                    </div>
-                  </div>
-                ))}
-              </div>
-            ) : (
-              ""
-            )}
           </AccordionSummary>
-
           <div className="question_boxes">
             <AccordionDetails className="add_question">
               <div className="add_question_top">
