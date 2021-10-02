4090e49aaad1095887a94165f66daa8adeb53d30
alienao <armin.orujyan@gmail.com>
2021-10-02 19:10:10 +0400

    refactor: changing css files with module.css files

--- src/components/formHeader/FormHeader.js
+++ src/components/formHeader/FormHeader.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./FormHeader.css";
+import styles from "./FormHeader.module.css";
 
 function FormHeader() {
   return (
-    <div className="form_header">
+    <div className={styles.form_header}>
       <h2 style={{ color: "grey" }}>Untitled form</h2>
     </div>
   );
