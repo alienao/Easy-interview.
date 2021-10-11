cbdedf373bf53ee784c9502e3e8ef3cf841606d7
alienao <armin.orujyan@gmail.com>
2021-10-11 15:00:42 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import Login from "./components/Login/Login";
-import Signup from "./components/Signup/Signup";
-import Quiz from "./components/Quiz/Quiz";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import User from "./components/User/User";
-import UserStart from "./components/UserStart/UserStart";
+import Login from './components/Login/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styles from './App.module.css';
 
 function App() {
   let jsonArr = [];
@@ -13,26 +10,16 @@ function App() {
   };
 
   return (
-    <Router>
-      <div>
+    <div className={styles.app}>
+      <Router>
         <Switch>
           <Route exact path="/">
-            {" "}
-            <Login userArr={jsonArr} />{" "}
+            {' '}
+            <Login userArr={jsonArr} userCreatorFunc={userCreator} />{' '}
           </Route>
-          <Route exact path="/signup">
-            {" "}
-            <Signup userCreatorFunc={userCreator} />{" "}
-          </Route>
-          <Route exact path="/userStart"><UserStart/></Route>
-          <Route path="/user"><User/></Route>
-          <Route exact patch="/quiz">
-            <Quiz />
-          </Route>
-          
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,138 +1,125 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import { Link } from 'react-router-dom';
-import Box from '@material-ui/core/Box';
-import FormControl from '@material-ui/core/FormControl';
-import Paper from '@material-ui/core/Paper';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import IconButton from '@material-ui/core/IconButton';
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
-import BgImg from '../../assets/images/background.jpeg';
-import names from '../../json/names.json'
-const useStyles = makeStyles((theme) => ({
-  background: {
-    backgroundImage: `url(${BgImg})`,
-    backgroundSize: 'cover',
-    height: window.innerHeight,
-    width: window.innerWidth,
-  },
-  loginElements: {
-    float: 'left',
-    marginLeft: '260px',
-    marginTop: '250px',
-  },
-  logo: {
-    fontSize: 30,
-    textAlign: 'center',
-    color: '#553E93',
-    fontFamily: 'sans-serif',
-  },
-  form: {
-    marginTop: theme.spacing(2.5),
-    display: 'flex',
-    flexDirection: 'column',
-    //alignItems:'center',
-  },
-  button: {
-    marginTop: theme.spacing(2),
-    margin: '0 auto',
-    display: 'flex',
-    color: '#553E93',
-    height: '35px',
-    width: '140px',
-    borderColor: '#553E93',
-  },
-  signupLink: {
-    textAlign: 'center',
-    marginTop: theme.spacing(2.5),
-  },
-}));
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import styles from './Login.module.css';
 
-const Login = (props) => {
-  const classes = useStyles();
+const theme = createTheme();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [visible, setVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    setVisible(!visible);
-  };
-
-  const handelEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handelPassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handelSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-    setEmail('');
-    setPassword('');
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
   return (
-    <Paper className={classes.background}>
-      <Box className={classes.loginElements}>
-        <Box className={classes.logo} style={{}}>
-          {' '}
-          {names.SignInLogo}
-        </Box>
-
-        <FormControl className={classes.form}>
-          <TextField
-            id="first"
-            label="Email-Address"
-            variant="filled"
-            margin="normal"
-            color="#553E93"
-            value={email}
-            onChange={handelEmail}
-          />
-
-          <TextField
-            id="second"
-            label="Password"
-            variant="outlined"
-            margin="normal"
-            color="#553E93"
-            type={visible ? 'text' : 'password'}
-            value={password}
-            onChange={handelPassword}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={toggleVisibility}>
-                    {visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                  </IconButton>
-                </InputAdornment>
-              ),
+    <Box className={styles.contMain}>
+      <ThemeProvider theme={theme}>
+        <Container component="main">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 10,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              bgcolor: '#cfe8fc',
+              justifyItems:"center"
             }}
-          />
-        </FormControl>
+          >
+            <Typography sx={{ mt: 4, mb: 2 }}>
+              <span className={styles.header}>Log In</span>
+            </Typography>
 
-        <Button
-          variant="outlined"
-          className={classes.button}
-          type="submit"
-          onClick={handelSubmit}
-        >
-          {names.SignIn}
-        </Button>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <InputLabel shrink htmlFor="email" sx={{ mt: 3, ml: 5, mr: 5 }}>
+                <span className={styles.email}>Email Address</span>
+              </InputLabel>
 
-        <Box className={classes.signupLink}>
-          {names.SignInDont}
-          <Link to="/signup"> {names.SignInhere}</Link> .
-        </Box>
-      </Box>
-    </Paper>
+              <TextField
+                className={styles.emailField}
+                sx={{ ml: 5, mr: 5 }}
+                required
+                style={{ width: 364 }}
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+
+              <InputLabel
+                shrink
+                htmlFor="password"
+                sx={{ mt: 3, ml: 5, mr: 5 }}
+              >
+                <span className={styles.password}>Password</span>
+              </InputLabel>
+
+              <TextField
+                sx={{ ml: 5, mr: 5 }}
+                style={{ width: 364 }}
+                required
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+
+              <FormControlLabel
+                sx={{ mt: 1.5, ml: 4, mr: 5 }}
+                control={<Checkbox value="remember" color="primary" />}
+                label={
+                  <span style={{ fontSize: '14px' }}>{'Remember me'}</span>
+                }
+              />
+
+              <Grid container>
+                <Grid item sx={{ mt: 1, ml: 5, mr: 5 }}>
+                  <span className={styles.createAcc}>
+                    {"Don't have an account.Create account "}
+                  </span>
+                  <Link
+                    href="#"
+                    variant="body2"
+                    style={{ textDecoration: 'none', color: '#4B63CB' }}
+                  >
+                    {' here'}
+                  </Link>
+                </Grid>
+              </Grid>
+
+              <Button
+                style={{ backgroundColor: '#4B63CB' }}
+                type="submit"
+                size="large"
+                variant="contained"
+                sx={{ mt: 3, mb: 2, ml: 5, mr: 5 }}
+              >
+                <span className={styles.btn}>Log In</span>
+              </Button>
+            </Box>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </Box>
   );
-};
-
-export default Login;
+}
